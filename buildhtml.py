5457f1b1df86b4b467547c146926bb136d09e434
leberte <35882232+leberte@users.noreply.github.com>
2019-08-31 12:51:15 +0800

    Delete buildhtml.py

--- buildhtml.py
+++
@@ -1,7 +0,0 @@
-#!/user/bin/python3
-# _*_ coding: utf-8 _*_
-from mako.template import Template
-def rederHtml(templateFile,postlist):
-    t = Template(filename=templateFile)
-    page = t.render(postlist = postlist )
-    return page
